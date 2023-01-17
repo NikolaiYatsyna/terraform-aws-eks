070796a7600f63e28425427f5b7141c30d1ec15b
Nikolai <nikolai.yatsyna@gmail.com>
2023-01-18 00:31:31 +0400

    Feature/cleanup (#6)

--- cluster.tf
+++ cluster.tf
@@ -9,7 +9,6 @@ module "eks" {
   cluster_endpoint_private_access = true
 
   eks_managed_node_group_defaults = {
-    enable_bootstrap_user_data = true
     attach_cluster_primary_security_group = true
     create_security_group = true
   }
