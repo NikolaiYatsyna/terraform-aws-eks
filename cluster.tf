a57fd709ff65e3296e167e5333d2ae381aa7384c
Nikolai <nikolai.yatsyna@gmail.com>
2023-01-18 11:52:56 +0400

    Enable CNI (#8)

--- cluster.tf
+++ cluster.tf
@@ -9,6 +9,26 @@ module "eks" {
   cluster_endpoint_private_access = true
   manage_aws_auth_configmap = true
 
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent              = true
+      service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
+      configuration_values = jsonencode({
+        env = {
+          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
+    }
+  }
+
   eks_managed_node_group_defaults = {
     attach_cluster_primary_security_group = true
     create_security_group = true
@@ -25,6 +45,22 @@ module "eks" {
       min_size     = var.nodegroup_min_size
       max_size     = var.nodegroup_max_size
       desired_size = var.nodegroup_desired_size
+    }
+  }
+}
+
+module "vpc_cni_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name_prefix      = "VPC-CNI-IRSA"
+  attach_vpc_cni_policy = true
+  vpc_cni_enable_ipv6   = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-node"]
     }
   }
 }
