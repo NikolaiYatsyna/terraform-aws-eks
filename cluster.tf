c884084f8f2243b90d830a1f468febc0b6c881cb
Nikolai <nikolai.yatsyna@gmail.com>
2023-01-18 11:21:13 +0400

    Feature/cleanup (#7)

--- cluster.tf
+++ cluster.tf
@@ -7,15 +7,18 @@ module "eks" {
   vpc_id     = var.vpc_id
   subnet_ids = var.subnets
   cluster_endpoint_private_access = true
+  manage_aws_auth_configmap = true
 
   eks_managed_node_group_defaults = {
     attach_cluster_primary_security_group = true
     create_security_group = true
+    iam_role_attach_cni_policy = true
   }
 
   eks_managed_node_groups = {
-    default = {
 
+    default_node_group = {
+      use_custom_launch_template = false
       name = var.cluster_name
       instance_types = [var.instance_type]
 
