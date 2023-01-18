d4f9092403b5ca553418a347ae23b55aac705d14
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-01-18 19:46:58 +0400

    enable CNI

--- cluster.tf
+++ cluster.tf
@@ -1,13 +1,14 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
+  source = "terraform-aws-modules/eks/aws"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
-  vpc_id     = var.vpc_id
-  subnet_ids = var.subnets
+  vpc_id                          = var.vpc_id
+  subnet_ids                      = var.subnets
   cluster_endpoint_private_access = true
-  manage_aws_auth_configmap = true
+  manage_aws_auth_configmap       = true
+  create_cni_ipv6_iam_policy      = true
 
   cluster_addons = {
     coredns = {
@@ -31,16 +32,16 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     attach_cluster_primary_security_group = true
-    create_security_group = true
-    iam_role_attach_cni_policy = true
+    create_security_group                 = true
+    iam_role_attach_cni_policy            = true
+
   }
 
   eks_managed_node_groups = {
-
     default_node_group = {
       use_custom_launch_template = false
-      name = var.cluster_name
-      instance_types = [var.instance_type]
+      name                       = var.cluster_name
+      instance_types             = [var.instance_type]
 
       min_size     = var.nodegroup_min_size
       max_size     = var.nodegroup_max_size
