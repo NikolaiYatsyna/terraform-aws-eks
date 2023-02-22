56cc430a0635389e6ff66cbdb1f2ccd22c969ea7
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-02-22 18:18:26 +0400

    Update code with var type (#20)

--- variables.tf
+++ variables.tf
@@ -1,33 +1,40 @@
 variable "stack" {
+  type        = string
   description = "stack name"
 }
 
 variable "cluster_version" {
+  type        = string
   description = "Amazon EKS Kubernetes version"
   default     = "1.24"
 }
 
 variable "instance_type" {
+  type        = string
   description = "Instance type"
   default     = "t3.micro"
 }
 
 variable "nodegroup_min_size" {
+  type        = string
   description = "Nodegroup min size"
   default     = "10"
 }
 
 variable "nodegroup_max_size" {
+  type        = string
   description = "Nodegroup max size"
   default     = "10"
 }
 
 variable "nodegroup_desired_size" {
+  type        = string
   description = "Nodegroup desired size"
   default     = "10"
 }
 
 variable "ami_id" {
+  type        = string
   description = "ami id for node group"
   default     = ""
 }
