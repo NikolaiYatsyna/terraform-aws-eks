ab0a6b0bb61ea482b6458eb5c67a7ec17a97cc91
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-02-27 21:37:12 +0400

    Expose OIDC issuer (#21)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "oidc_provider_arn" {
   description = "ARN of cluster OIDC provider"
   value       = module.eks.oidc_provider_arn
 }
+
+output "oidc_provider" {
+  description = "URL of cluster OIDC provider"
+  value       = module.eks.oidc_provider
+}
