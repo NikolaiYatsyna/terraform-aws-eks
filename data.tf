12ec39f90f8e684ffdc8a8c5d30773af628e0884
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-03-02 11:12:35 +0400

    Refactor data lookups and outputs

--- data.tf
+++ data.tf
@@ -7,18 +7,3 @@ data "aws_ami" "eks_default" {
     values = ["amazon-eks-node-${var.cluster_version}-v*"]
   }
 }
-
-data "aws_vpc" "vpc" {
-  filter {
-    name   = "tag:Name"
-    values = ["${var.stack}-vpc"]
-  }
-}
-
-data "aws_subnets" "node_subnets" {
-  tags = var.private_subnet_tags
-}
-
-data "aws_subnets" "control_plane_subnets" {
-  tags = var.intra_subnet_tags
-}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
 output "oidc_provider_arn" {
   description = "ARN of cluster OIDC provider"
   value       = module.eks.oidc_provider_arn
