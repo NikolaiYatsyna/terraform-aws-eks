bc561183f7f12c4eee8963bf0920bdaffe9832de
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-03-07 20:08:07 +0400

    Extend permissons
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- iam.tf
+++ iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_policy" "ebs_csi_policy" {
+resource "aws_iam_policy" "eks-node-policy" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -7,7 +7,33 @@ resource "aws_iam_policy" "ebs_csi_policy" {
           "ec2:CreateTags",
           "ec2:CreateVolume",
           "ec2:AttachVolume",
-          "ec2:DetachVolume"
+          "ec2:DetachVolume",
+
+          # DynamoDB for backend
+          "dynamodb:DescribeLimits",
+          "dynamodb:DescribeTimeToLive",
+          "dynamodb:ListTagsOfResource",
+          "dynamodb:DescribeReservedCapacityOfferings",
+          "dynamodb:DescribeReservedCapacity",
+          "dynamodb:ListTables",
+          "dynamodb:BatchGetItem",
+          "dynamodb:BatchWriteItem",
+          "dynamodb:CreateTable",
+          "dynamodb:DeleteItem",
+          "dynamodb:GetItem",
+          "dynamodb:GetRecords",
+          "dynamodb:PutItem",
+          "dynamodb:Query",
+          "dynamodb:UpdateItem",
+          "dynamodb:Scan",
+          "dynamodb:DescribeTable",
+
+          # KMS for Vault auto unseal
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey",
         ]
         Effect   = "Allow"
         Resource = "*"
